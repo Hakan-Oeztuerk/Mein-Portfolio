7cdf26032358f3dca517f7b7f2925bb8fd273248
Hakan Öztürk <hakanoezuerk@gmail.com>
2025-11-11 14:30:20 +0100

    ngrok und vercel installiert

--- src/assets/HeroWithVideo.jsx
+++ src/assets/HeroWithVideo.jsx
@@ -6,19 +6,20 @@ export default function HeroWithVideo() {
     return (
         <div className="relative w-full h-screen overflow-hidden">
             <video
-                className="absolute top-0 left-0 w-full h-full brightness-25 object-cover"                src={video}
+                className="absolute top-0 left-0 w-full h-200 brightness-25 object-cover"                src={video}
                 muted
                 autoPlay
                 loop
                 playsInline
                 preload="auto"
             />
-            <div className="relative z-2 flex flex-col items-center justify-center h-full text-white bg-opacity-50">
-                <h1 className="text-4xl md:text-6xl font-bold mb-4">
+            <div className="relative z-2 flex flex-col items-center  h-full text-white bg-opacity-50">
+                <h1 className="text-4xl md:text-6xl font-bold mt-30 mb-20">
                     Willkommen auf meiner Webseite!
                 </h1>
-                <img src={Profil} className="w-48 rounded-2xl mb-4" alt="Mein Bild" />
-                <p className="text-lg md:text-2xl">Hi, ich bin Hakan.</p>
+                <img src={Profil} className="w-60 rounded-2xl mb-4" alt="Mein Bild" />
+                <h5 className="text-lg md:text-4xl mt-6">Hi, ich bin Hakan.</h5>
+                <p className="text-md md:text-2xl mt-2 text-center px-4">Digital Product Designer & Webentwickler</p>
             </div>
         </div>
     );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite'
+import { ngrok } from 'vite-plugin-ngrok'
+import process from 'node:process'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  
+  return {
+    plugins: [react(), tailwindcss(),
+    
+      // ngrok({
+      //   authtoken: env.NGROK_AUTH_TOKEN      })
+    ],
+    server: {
+      allowedHosts: ['.ngrok-free.app'],
+    }
+  }
 })
