b1518aa3cdd51d7de9018509896a07747fe78b66
Hakan Öztürk <hakanoezuerk@gmail.com>
2025-11-14 12:23:06 +0100

    ngrok uninstall

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig, loadEnv } from 'vite'
-import { ngrok } from 'vite-plugin-ngrok'
 import process from 'node:process'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+})
 
   
-  return {
-    plugins: [react(), tailwindcss(),
-    ],
-  }
-})
